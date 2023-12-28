70a36a25e7680d47cc6e8f4415d8ca984086c8d2
GiulianaCravotta <giulianacravotta@gmail.com>
2023-12-28 20:07:22 -0300

    Desafio Entregable Clase 4

---
+++ ClaseManejoDeArchivosEnJavascript/ProductManager2.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+
+class ProductManager {
+    constructor(filePath) {
+        this.path = filePath;
+        this.products = [];
+        this.productIdCounter = 1;
+
+        // carga los productos
+        this.loadProducts();
+    }
+
+    addProduct(product) {
+        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
+            console.log("Todos los campos son obligatorios");
+            return;
+        }
+        if (this.products.some(p => p.code === product.code)) {
+            console.error("Ya existe un producto con el mismo código");
+            return;
+        }
+        const newProduct = {
+            id: this.productIdCounter++,
+            ...product
+        };
+        
+        this.products.push(newProduct);
+        this.saveProducts();
+        console.log(`Se ha agregado el producto ${newProduct.title}`);
+    }
+    getProducts() {
+        return this.products;
+    }
+    getProductById(id) {
+        const product = this.products.find(p => p.id === id);
+        if (!product) {
+            console.error(`Producto no encontrado con el id: ${id}`);
+            return;
+        }
+        return product;
+    }
+    updateProduct(id, updatedFields) {
+        const index = this.products.findIndex(p => p.id === id);
+        if (index === -1) {
+            console.error(`Producto no encontrado con el id: ${id}`);
+            return;
+        }
+
+        this.products[index] = {
+            ...this.products[index],
+            ...updatedFields
+        };
+        this.saveProducts();
+        console.log(`Producto actualizado con éxito`);
+    }
+    deleteProduct(id) {
+        const index = this.products.findIndex(p => p.id === id);
+        if (index === -1) {
+            console.error(`Producto no encontrado con el id: ${id}`);
+            return;
+        }
+
+        this.products.splice(index, 1);
+        this.saveProducts();
+        console.log(`Producto id ${id} eliminado con éxito`);
+    }
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            this.products = JSON.parse(data);
+            if (this.products.length > 0) {
+                this.productIdCounter = Math.max(...this.products.map(p => p.id)) + 1;
+            }
+        } catch (error) {
+            this.products = [];
+        }
+    }
+    saveProducts() {
+        try {
+            const data = JSON.stringify(this.products, null, 2);
+            fs.writeFileSync(this.path, data, 'utf8');
+        } catch (error) {
+            console.error('Error al guardar productos en el archivo:', error.message);
+        }
+    }
+}
+
+module.exports = ProductManager;
---
+++ ClaseManejoDeArchivosEnJavascript/main2.js
@@ -0,0 +1,34 @@
+const ProductManager = require('./ProductManager2'); 
+
+// archivo JSON
+const filePath = './productos.json';
+
+//
+const productManager = new ProductManager(filePath);
+
+//arreglo vacio
+console.log(productManager.getProducts());
+
+// agregar un producto
+productManager.addProduct({ title: "Producto 1", description: "Producto Prueba 1", price: 123, thumbnail: "Sin imagen", code: 543, stock: 10 });
+
+// productos después de agregar uno
+console.log('Productos después de agregar uno:', productManager.getProducts());
+
+//producto por ID
+const productId = 1; 
+console.log(`Producto con ID ${productId}:`, productManager.getProductById(productId));
+
+// actualizar un producto
+const updatedProductId = 1; 
+productManager.updateProduct(updatedProductId, { price: 250, stock: 30 });
+
+// productos después de la actualización
+console.log('Productos después de la actualización:', productManager.getProducts());
+
+// eliminar un producto
+const deletedProductId = 1; 
+productManager.deleteProduct(deletedProductId);
+
+//productos después de la eliminación
+console.log('Productos después de la eliminación:', productManager.getProducts());
